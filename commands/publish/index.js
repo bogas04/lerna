eed401af7661b2b6d21ae7c13b20cda1dfd84a45
Daniel Stockman <daniel.stockman@gmail.com>
2018-08-08 15:19:39 -0700

    feat(publish): Support 2FA during publish (WIP)
    TODO: unit tests (direct + high-level)
    
    refs #1091

--- commands/publish/index.js
+++ commands/publish/index.js
@@ -37,6 +37,8 @@ const getPackagesWithoutLicense = require("./lib/get-packages-without-license");
 const gitCheckout = require("./lib/git-checkout");
 const removeTempLicenses = require("./lib/remove-temp-licenses");
 const verifyNpmPackageAccess = require("./lib/verify-npm-package-access");
+const getTwoFactorAuthRequired = require("./lib/get-two-factor-auth-required");
+const promptOneTimePassword = require("./lib/prompt-one-time-password");
 
 module.exports = factory;
 
@@ -408,6 +410,10 @@ class PublishCommand extends Command {
     // if no username was retrieved, don't bother validating
     if (this.conf.get("username") && this.verifyAccess) {
       chain = chain.then(() => verifyNpmPackageAccess(this.packagesToPublish, this.conf.snapshot));
+      chain = chain.then(() => getTwoFactorAuthRequired(this.conf.snapshot));
+      chain = chain.then(isRequired => {
+        this.twoFactorAuthRequired = isRequired;
+      });
     }
 
     return chain;
@@ -509,6 +515,14 @@ class PublishCommand extends Command {
       });
   }
 
+  requestOneTimePassword() {
+    return Promise.resolve()
+      .then(() => promptOneTimePassword())
+      .then(otp => {
+        this.conf.set("otp", otp);
+      });
+  }
+
   packUpdated() {
     const tracker = this.logger.newItem("npm pack");
 
@@ -566,6 +580,10 @@ class PublishCommand extends Command {
 
     let chain = Promise.resolve();
 
+    if (this.twoFactorAuthRequired) {
+      chain = chain.then(() => this.requestOneTimePassword());
+    }
+
     const opts = this.conf.snapshot;
     const mapper = pPipe(
       [
@@ -599,6 +617,12 @@ class PublishCommand extends Command {
     tracker.showProgress();
 
     let chain = Promise.resolve();
+
+    // there's no reasonable way to guess if the OTP has expired already,
+    // so we have to ask for it every time
+    if (this.twoFactorAuthRequired) {
+      chain = chain.then(() => this.requestOneTimePassword());
+    }
 
     const opts = this.conf.snapshot;
     const mapper = pkg => {
